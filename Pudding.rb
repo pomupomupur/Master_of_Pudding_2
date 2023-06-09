bb602a34050cf8277b9fcb49156e726c0b474899
tie <dolphin0@outlook.jp>
2023-06-09 16:37:12 +0900

    なんか

--- Pudding.rb
+++ Pudding.rb
@@ -13,15 +13,15 @@ Window.width = 800
 Window.height = 600
 
 # 画像の読み込み
-taiko_image = Image.load('picture/taiko.jpg')
-note_image = Image.load('picture/pomu.jpg')
-background_image = Image.load('picture/haikei.png')
+taiko_image = Image.load('picture/taiko2.jpeg')
+note_image = Image.load('picture/pomu2.jpeg')
+background_image = Image.load('picture/haikei2.jpeg')
 
 # 太鼓の位置とノーツの速度
-taiko_y = Window.height - taiko_image.height
+taiko_y = 200
+note_speed = 7
 note_x = Window.width
-note_y = Window.height - note_image.height
-note_speed = 5
+note_y = taiko_y - note_image.height
 
 # ゲームループ
 Window.loop do
@@ -29,10 +29,11 @@ Window.loop do
   Window.draw(0, 0, background_image)
 
   # 太鼓の描画
-  Window.draw(100, taiko_y, taiko_image)
+  Window.draw(200, taiko_y, taiko_image)
 
   # ノーツの描画
-  notes.each do |note|
+  if index < 100
+    note = notes[index]
     if note == 1
       Window.draw(note_x, note_y, note_image)
     end
@@ -40,16 +41,17 @@ Window.loop do
 
     if note_x <= 0
       note_x = Window.width
+      index += 1  # 次のノーツへ進む
     end
 
     # キー入力の受け付けと判定
-    if Input.key_push?(K_SPACE) && note_y >= taiko_y && note_y <= taiko_y + taiko_image.height
+    if Input.key_push?(K_SPACE) && note_x <= 200 && note_x >= 200 - note_image.width
       score += 100
     end
   end
 
   # ゲーム情報の描画
-  Window.draw_font(10, 10, '太鼓の達人', Font.default)
+  Window.draw_font(10, 10, '太鼓の達人', Font.default, color:[0,0,0])
   Window.draw_font(10, 40, 'ノーツの数：' + notes.size.to_s, Font.default)
   Window.draw_font(10, 70, '現在のスコア：' + score.to_s, Font.default)
 end
